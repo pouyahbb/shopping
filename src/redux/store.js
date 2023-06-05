b73e22db260c31ab43a7c8279d77c8bc9c13476d
pouyaHobbi <pouyahobbi504@gmail.com>
2023-06-05 17:59:29 +0430

    Add redux store and user actions

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export default store;
